f0d0ae66183144dc58181243313d0151afe56573
nandakishore reddy <nandakishorereddykamatham@gmail.com>
2023-10-27 22:55:16 -0400

    remove tab functioanlity and add home page functioanlity

--- src/home.js
+++ src/home.js
@@ -1,9 +1,20 @@
-console.log('lofasd')
+const home=()=>{
+    const content =document.querySelector("#content")
+    const pageContent=document.createElement('div')
+    pageContent.classList.add('page-content')
 
-function children(){
-    var newChild=document.getElementById('content')
-newChild.innerHTML="hello jalepenos"
-document.appendChild(newChild)
+    const image=document.createElement('img')
+    image.src="https://i.pinimg.com/736x/56/25/18/562518009aa9a63d6c490a254ee53796.jpg"
+    pageContent.addChild(image)
+
+
+    const headline=document.createElement('h1')
+    headline.innerHTML="Indian Aesthetics"
+    pageContent.addChild('headline')
+
+    const text=document.createElement('p')
+    text.innerHTML="World's famous Hyderbadi biryani"
+    pageContent.addChild('text')
+    content.appendChild('pageContent')
 }
-children()
-    // return newChild
+export default home
---
+++ src/pageLoad.js
@@ -0,0 +1,10 @@
+import home from './home'
+function pageLoad(){
+    home()
+    contact()
+    menu()
+    tabs()
+
+}
+
+export default pageLoad
